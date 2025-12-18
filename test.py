d65106bcadf5c07325ca3f50de5ec1e7dde0ea5e
Rat1l <nazarkupranec@gmail.com>
2025-12-18 15:12:48 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
-import telebot
-TOKEN = "8254517535:AAH_8Xu1ZUMhrI7e93tl8SQg3PxYzIG5uj4"
-bot = telebot.TeleBot(TOKEN)
-@bot.message_handler(commands = ['a'])
-def MICRO(message):
-    bot.send_message(message.chat.id, "член")
-bot.polling()
+
